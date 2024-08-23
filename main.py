6c8d6b707b6fe46bd547b97bdc6228114b9910e3
Ojaswa Sharma <93905595+Sowoul@users.noreply.github.com>
2024-08-23 11:23:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -139,4 +139,4 @@ def handle_move(data):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    socket.run(app=app, port=8000, debug=True)
+    socket.run(app=app, host='0.0.0.0', port=8000, debug=True)
